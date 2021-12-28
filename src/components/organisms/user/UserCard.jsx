acc118658073cd73055fd72f584eed301a204c1f
hekushi <78855499+hekushi@users.noreply.github.com>
2021-12-29 08:05:12 +0900

    add1229

--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,21 +1,22 @@
 import styled from "styled-components";
+import { Card } from "../../atoms/card/card";
+import { UserIconWithName } from "../../molecules/user/UserIconWithName";
 export const UserCard = (props) => {
   const { user } = props;
   return (
-    <div>
-      <img height={160} width={160} src={user.image} alt="プロフィール" />
-      <p>名前</p>
+    <Card>
+      <UserIconWithName image={user.image} name={user.name} />
       <SDl>
         <dt>メール</dt>
-        <dd>1111@aaa.com</dd>
+        <dd>{user.email}</dd>
         <dt>TEL</dt>
-        <dd>000-1111-2222</dd>
+        <dd>{user.phone}</dd>
         <dt>会社名</dt>
-        <dd>あいうえお株式会社</dd>
+        <dd>{user.company.name}</dd>
         <dt>WEB</dt>
-        <dd>htttttt.inc</dd>
+        <dd>{user.website}</dd>
       </SDl>
-    </div>
+    </Card>
   );
 };
 
@@ -28,5 +29,6 @@ const SDl = styled.dl`
   dd {
     padding-left: 32px;
     padding-bottom: 8px;
+    overflow-wrap: break-word;
   }
 `;
