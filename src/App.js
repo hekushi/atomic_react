3fbefa184cf2e1264a4fa967507a65fbf9e5934f
hekushi <78855499+hekushi@users.noreply.github.com>
2021-12-31 08:13:54 +0900

    add1231

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "じゃけぇ",
-  image: "https://source.unsplash.com/g2E2NQ5SWSU",
-  email: "12345@example.com",
-  phone: "090-1111-1222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://www.yahoo.co.jp/"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>sssss</PrimaryButton>
-        <SecondaryButton>sssss</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
